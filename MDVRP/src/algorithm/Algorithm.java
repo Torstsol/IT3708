1104a99d0c6e2d3fd7262dda932eb4a356a5fe90
Torstein Solberg <torstsol@stud.ntnu.no>
2020-01-22 13:58:24 +0100

    added method for generating population

---
+++ MDVRP/src/algorithm/Algorithm.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Algorithm {
+
+    public ArrayList<ArrayList<ArrayList<Integer>>> seedPopulation(ArrayList<ArrayList<Integer>> pseudoChromosome, int populationSize){
+
+        ArrayList<ArrayList<ArrayList<Integer>>> population = new ArrayList<ArrayList<ArrayList<Integer>>>();
+
+        for(int i = 0; i < populationSize; i++){
+            ArrayList<ArrayList<Integer>> chromosome = new ArrayList<ArrayList<Integer>>();
+            for (ArrayList<Integer> subPseudoChromosome: pseudoChromosome){
+                // copy the array to avoid reference bug
+                ArrayList<Integer> copy = new ArrayList<Integer>(subPseudoChromosome);
+                Collections.shuffle(copy);
+                chromosome.add(copy);
+            }
+            population.add(chromosome);
+        }
+        return population;
+    }
+}
